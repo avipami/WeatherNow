b6d601328cbc991ec4ee280644694b1e3550b252
Vincent <35411050+avipami@users.noreply.github.com>
2022-09-19 12:20:24 +0200

    Initial Commit

---
+++ WeatherNow/WeatherNowApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherNowApp.swift
+//  WeatherNow
+//
+//  Created by Vincent Palma on 2022-09-19.
+//
+
+import SwiftUI
+
+@main
+struct WeatherNowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
